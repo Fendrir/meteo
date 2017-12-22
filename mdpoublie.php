99082d2f1200c20f2c1fc6a50694f39543d1465a
Fendrir <papao1@hotmail.fr>
2017-12-22 09:40:03 +0100

    changement de type de bdd, migration de postgre sql a my sql

--- mdpoublie.php
+++ mdpoublie.php
@@ -2,25 +2,54 @@
 
 var_dump($_POST);
 
+// connection My sql
 
-//connexion bdd locale
-$test = "host=localhost port=5432 dbname=meteon user=johanna password=gaelle";
+$servername = "localhost";
+$username = "root";
+$password = "admin";
+$dbname = "meteon";
 
-$connect = pg_connect($test);
-
+try    
+{
+    $bdd = new PDO('mysql:host='.$servername.';dbname='.$dbname.';charset=utf8', ''.$username.'', ''.$password.'');
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+};
 
 $mail=$_POST['mail'];
 
-$foo = pg_query("SELECT nom, prenom FROM utilisateur WHERE  mail='".$mail."';");
-$resultat = pg_fetch_array($foo);
-
-var_dump($foo);
+$reponse = $bdd->query('SELECT uti_nom, uti_prenom FROM utilisateur WHERE uti_mail="'.$mail.'";');
 
 
+while ($donnees = $reponse->fetch()) 
+    {
+        
+        mail($mail,"Mot de passe oublie","Voici ton mot de passe : $donnees[0]$donnees[1]");
+           
+    }
 
-var_dump(mail($mail,"Mot de passe oublie","Voici ton mot de passe : $resultat[0]$resultat[1]"));
+    $reponse->closeCursor();
 
-pg_close($connect);
+//connexion bdd locale postgre sql
+// $test = "host=localhost port=3306 dbname=meteon user=root password=admin";
+
+// $connect = pg_connect($test);
+
+
+// $mail=$_POST['mail'];
+
+// $foo = pg_query("SELECT nom, prenom FROM utilisateur WHERE  mail='".$mail."';");
+// $resultat = pg_fetch_array($foo);
+
+// var_dump($foo);
+
+
+
+// var_dump(mail($mail,"Mot de passe oublie","Voici ton mot de passe : $resultat[0]$resultat[1]"));
+
+// pg_close($connect);
 //bouton deconnexion
 
 
@@ -31,6 +60,13 @@ pg_close($connect);
 <head>
 </head>
 <body>
-<p> <?php echo "$resultat[0]$resultat[1]";?> </p>
+<p>
+
+    <?php // echo "$resultat[0]$resultat[1]";
+        echo "$donnees[0]$donnees[1]";
+
+    ?>
+
+</p>
 </body>
 </html>
